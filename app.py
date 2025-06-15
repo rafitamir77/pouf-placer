d26dca3170af78232f27a46a8dbb3bd26eca4420
rafitamir77 <rafi.tamir@gmail.com>
2025-06-15 07:48:59 +0300

    10

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ if uploaded_file:
     display_height = int(display_width * aspect_ratio)
     resized_room = room_image.resize((display_width, display_height))
 
+    print(f'aspect_ratio {aspect_ratio}.')
+    print(f'display_width {aspect_ratio}.')
+    print(f'display_height {display_height}.')
+
     if "last_image" in st.session_state:
         # Show latest image with pouf
         resized_room = st.session_state["last_image"].resize((display_width, display_height))
