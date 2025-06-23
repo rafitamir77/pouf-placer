50fef731755b99a007c065e83072e43630d87c93
rafitamir77 <rafi.tamir@gmail.com>
2025-06-23 13:03:49 +0300

    temp

--- app.py
+++ app.py
@@ -135,14 +135,7 @@ if uploaded_file:
         # Resize for realism
         shadow_x = x_pos + int((pouf_width - shadow_size[0]) / 2)
         shadow_y = y_pos + int(pouf_height * 0.85)
-        
-        x_pos = 100
-        y_pos = 100
-        shadow_x= 100
-        shadow_y=100
-        overlay.paste(blurred_shadow, (shadow_x, shadow_y), blurred_shadow    )
-
-
+        overlay.paste(blurred_shadow, (shadow_x, shadow_y),     )
         overlay.paste(scaled_pouf, (x_pos, y_pos), mask=scaled_pouf)
 
         result = Image.alpha_composite(room_image, overlay)
